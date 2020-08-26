75bb4c40f98518de8173b6d013a4aaaaef695d99
xielinwei <964015668@qq.com>
2020-08-26 08:54:39 +0800

    company

--- promise/async.awiat.js
+++ promise/async.awiat.js
@@ -1,3 +1,7 @@
+// async 相当于 new Promise()
+// await 相当于返回一个 Promise.resolve()
+// await就是then的语法糖
+
 async function queue() {
     let a = await new Promise((resolve) => {
         setTimeout(() => {
@@ -6,4 +10,13 @@ async function queue() {
     });
     console.log(a)
 }
-queue();
+
+
+async function xlw() {
+  let a = await "22";
+  console.log(a)
+  return Promise.resolve("ss")
+}
+console.log("11")
+xlw().then(v => console.log(v))
+console.log(33)
