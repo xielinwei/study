2b965a9f1c07d63997fee838d6e063f3622df53e
lisi <lisi@123.com>
2020-12-11 19:34:49 +0800

    电磁辐射大三

---
+++ vue源码/虚拟dom/webpack.config.js
@@ -0,0 +1,10 @@
+let path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "index.js",
+        path: path.resolve(__dirname, "dist")
+    }
+}
