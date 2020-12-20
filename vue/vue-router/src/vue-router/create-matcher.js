0c4a5991236597720aa2c79a00c710d881a1258a
xielinwei <964015668@qq.com>
2020-12-20 18:48:30 +0800

    fkl;asd

--- vue/vue-router/src/vue-router/create-matcher.js
+++ vue/vue-router/src/vue-router/create-matcher.js
@@ -4,7 +4,7 @@ export default function createMatcher(routes){
 
   // 动态添加路由的方法
   function addRoutes(){
-
+    createRouteMap(routes, pathList, pathMap)
   }
 
   function match(){
---
+++ vue/vue-router/src/vue-router/create-route-map.js
@@ -0,0 +1,3 @@
+export default function createRouteMap(routes, oldPathList, oldPathMap){
+
+}
